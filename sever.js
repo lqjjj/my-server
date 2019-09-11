84fe12460a27657f5aef4251025fd74b253fff05
tao.you <tao.you@ucarinc.com>
2019-09-11 17:05:41 +0800

    blog sever version 2.0

--- sever.js
+++ sever.js
@@ -14,17 +14,20 @@ db.then(()=>{
     app.get('/',(request,res)=>{
         res.sendFile(__dirname+'/dist')
     });
-    app.get('/api/blog',async (request,res)=>{
-        let result = await blogmodel.find({author:'小游'}).sort({'_id':-1})
+    app.get('/api/blog/:path',async (req,res)=>{
+        let result;
+        if (req.params.path==='all') {
+            result = await blogmodel.find({author:'小游'}).sort({'_id':-1});
+        }
+        else {
+            result = await blogmodel.findOne({_id: req.params.path}).select({title:1,content:1,color:1});
+        }
         res.send(result);
     });
     app.get('/api/img/:path',(req,res)=>{
-        console.log(req.params.path);
         res.sendFile(__dirname+'/uploads/'+req.params.path)
     }),
-    app.post('/api/addblog',multer.array('file'),async (request,res)=>{
-        // console.log(request.body);
-        // console.log(request.files);
+    app.post('/api/add_blog',multer.array('file'),async (request,res)=>{
         let {title,author,content,color}=request.body;
         let img_list=request.files.map((item)=>{
             return "/api/img/"+item['filename']
@@ -33,6 +36,12 @@ db.then(()=>{
         console.log(blogmodel);
         res.send('ok')
     });
+    app.post('/api/edit_text',async (req,res)=>{
+        let {_id,title,content,color}=req.body;
+        await blogmodel.updateOne({_id:_id},{title,content,color,edited_time:''});
+        res.send('ok')
+    })
+
 }).catch((err)=>{
     console.log('数据库链接失败',err)
 });
