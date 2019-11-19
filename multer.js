b9f3e0f5a54833f88de8d28a09ebaf868adabe1a
tao.you <tao.you@ucarinc.com>
2019-11-19 16:37:15 +0800

    blog sever version 3.0

--- multer.js
+++ multer.js
@@ -1,11 +1,11 @@
 let multer=require('multer');
 let storage=multer.diskStorage({
     destination:function (req,file,cb) {
-        cb(null,'./uploads')
+        cb(null,'/opt/git/my-server/uploads')
     },
     filename:function (req,file,cb) {
         let arr=file.originalname.split('.');
-        cb(null,arr[0]+' - '+new Date().toLocaleDateString()+'.'+arr[1]);
+        cb(null,new Date().toLocaleDateString()+file.originalname);
     }
 });
 
